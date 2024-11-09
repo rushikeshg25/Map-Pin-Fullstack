da177f1c1f4e9573b4c833aac5a47ceaaee6bd28
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-11-09 11:36:58 +0530

    red: unused import

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import "./app.css";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
-import { useEffect, useState } from "react";
-import { Room, Star, StarBorder } from "@material-ui/icons";
+import { Room, Star } from "@material-ui/icons";
 import axios from "axios";
+import { useEffect, useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { format } from "timeago.js";
-import Register from "./components/Register";
+import "./app.css";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   const myStorage = window.localStorage;
